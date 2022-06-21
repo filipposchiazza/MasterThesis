1f06108a82c60d0fac8145da7a7224e880e24d08
filipposchiazza <filipposchiazza97@gmail.com>
2022-06-21 12:21:49 +0200

    Implement the separation of MNIST in training, validation, test

--- MNIST/train_VAE_mnist.py
+++ MNIST/train_VAE_mnist.py
@@ -1,5 +1,8 @@
+import numpy as np
 from vae import VAE
 from tensorflow.keras.datasets import mnist
+from sklearn.model_selection import train_test_split
+
 
 
 LEARNING_RATE = 0.0005
@@ -13,15 +16,22 @@ N_EPOCHS = [30, 40, 50, 60]
 LATENT_SPACE_DIM = [3, 5, 10, 20, 50]
 
 
-def load_mnist():
+def load_mnist(train_ratio=0.6, validation_ratio=0.2, test_ratio=0.2):
+
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # concatenate together all data and targets
+    x = np.concatenate([x_train, x_test])
+    y = np.concatenate([y_train, y_test])
     # normalization
-    x_train = x_train.astype("float32") / 255
-    x_test = x_test.astype("float32") / 255
+    x = x.astype("float32") / 255
     # add channel dimension (1 for gray scale images)
-    x_train = x_train.reshape(x_train.shape + (1,))
-    x_test = x_test.reshape(x_test.shape + (1,))
-    return x_train, y_train, x_test, y_test
+    x = x.reshape(x.shape + (1,))
+    # obtain train dataset
+    x_train, x_remain, y_train, y_remain = train_test_split(x, y, train_size=train_ratio)
+    # divide validation and test dataset
+    x_val, x_test, y_val, y_test = train_test_split(x_remain, y_remain, train_size=(validation_ratio/(validation_ratio+test_ratio)))
+    return x_train, y_train, x_val, y_val, x_test, y_test    
+
 
 
 
@@ -42,7 +52,7 @@ def train(x_train, learning_rate, batch_size, epochs, latent_dim, kl_weight):
 
 
 if __name__ == '__main__':
-    x_train, _, _, _ = load_mnist()
+    x_train, _, _, _, _, _ = load_mnist()
     
     # Train the model for different values of the kl_weight
     for kl in KL_WEIGHTS:
